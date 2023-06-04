0f25e20534a622793b2312bc69bdb2577b5eaa7c
cremirdevio <josephajibodu@gmail.com>
2023-06-04 20:39:09 +0100

    chore: airbnb eslint config installed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,13 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'airbnb',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 'off',
   },
-}
+};
