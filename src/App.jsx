9489d6c4bb460ae952eb694a5270fb4c52efdc70
cremirdevio <josephajibodu@gmail.com>
2023-06-04 19:48:29 +0100

    chore: starting template

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 function App() {
   // Router implementation here
-  return <div className="flex text-3xl">QwikBill</div>;
+  return (
+    <div className="flex text-3xl w-full bg-orange-500 h-screen md:w-[700px]">
+      QwikBill
+    </div>
+  );
 }
 
 export default App;
