07df6fdcaabd172cce819f68947745256e09c7ad
Yinka <asyinka@gmail.com>
2023-06-06 21:26:38 +0100

    chore: replace props with 'spread props'

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,21 +1,20 @@
 import PropTypes from "prop-types";
 
-export default function Input({ type, placeholder }) {
+export default function Input({ type, ...props }) {
   return (
     <input
       type={type}
-      placeholder={placeholder}
-      className="rounded border-[1px] border-solid border-black/40 text-sm p-1 h-fit outline-none disabled:cursor-not-allowed disabled:opacity-50"
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      className="rounded inline-block border-[1px] border-solid border-black/40 text-base p-4 h-11 outline-none disabled:cursor-not-allowed disabled:opacity-50"
     />
   );
 }
 
 Input.propTypes = {
   type: PropTypes.string,
-  placeholder: PropTypes.string,
 };
 
 Input.defaultProps = {
   type: "default",
-  placeholder: "string",
 };
