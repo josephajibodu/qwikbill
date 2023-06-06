1426eb09e903b509b1b69b67b804128de7dca0b3
Yinka <asyinka@gmail.com>
2023-06-06 01:54:27 +0100

    feat: add button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Badge from "./components/ui/badge";
+import Button from "./components/ui/button";
 
 function App() {
   // Router implementation here
   return (
     <div className="flex text-3xl w-full bg-orange-500 h-screen md:w-[700px]">
       <Badge variant="solid" />
+      <Button variant="outline">Sign in</Button>
     </div>
   );
 }
---
+++ src/components/ui/button.jsx
@@ -0,0 +1,37 @@
+// import { ReactPropTypes } from "react";
+import PropTypes from "prop-types";
+
+export default function Button({ variant, children }) {
+  let bgColor;
+
+  switch (variant) {
+    case "solid":
+      bgColor = "bg-teal-500";
+      break;
+    case "outline":
+      bgColor = "bg-transparent border-2 border-solid border-teal-500";
+      break;
+    default:
+      bgColor = "bg-teal-500";
+      break;
+  }
+
+  return (
+    <button
+      type="button"
+      className={`mt-2 text-base cursor-pointer font-bold text-white rounded-[32px] h-max w-full py-4 ${bgColor}`}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  variant: PropTypes.string,
+  children: PropTypes.string,
+};
+
+Button.defaultProps = {
+  variant: "solid",
+  children: "default",
+};
