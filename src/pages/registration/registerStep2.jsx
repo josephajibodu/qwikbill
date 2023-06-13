40c02ef5e4383d4c69713e8cd3c97a65857debca
Yinka <asyinka@gmail.com>
2023-06-13 21:36:25 +0100

    feat: add registration page for email and confirmation

--- src/pages/registration/registerStep2.jsx
+++ src/pages/registration/registerStep2.jsx
@@ -5,15 +5,15 @@ import StepperIndicator from "../../components/stepperIndicator";
 
 export default function RegisterStep2() {
   return (
-    <div className="flex flex-col bg-white h-full px-8">
+    <div className="flex flex-col bg-white h-full px-8 font-secondary">
       <StepperIndicator currStep="2" />
       <h2 className="font-secondary text-xl font-bold text-gray-700">
         Confirm Email Address
       </h2>
       <div className="h-10 mt-2.5 mb-[18px]">
-        <p className="inline font-secondary text-base font-light text-gray-600">
+        <p className="font-secondary text-base font-light text-gray-600">
           Please enter the code sent to the email address <nbsp />
-          <b className="inline text-base font-bold">johndoe@example.com</b>
+          <b className="text-base font-bold">johndoe@example.com</b>
         </p>
       </div>
       <Label htmlFor="e-mail">Enter OTP</Label>
@@ -21,9 +21,11 @@ export default function RegisterStep2() {
       <Button variant="solid" size="lg" className="mt-[100px]">
         Continue
       </Button>
-      <div className="bg-red-500 text-center">
+      <div className=" mt-6 text-xs font-bold text-center text-gray-600">
         <b>Didnt receive a code?</b>
-        <span>Resend</span>
+        <a href="##" className="rounded-md ml-1 bg-gray-300 p-2">
+          Resend
+        </a>
       </div>
     </div>
   );
